817b4c737ee2b8aacb7d9aaf83a8c1bd2698a13d
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-03-26 12:17:23 +0700

    Add result

---
+++ submit_train_T12a.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -p gpu				# Specify partition [Compute/Memory/GPU]
+#SBATCH -N 1 -c 16          		# Specify number of nodes and processors per task
+#SBATCH --gpus-per-node=1               # Specify the number of GPUs
+#SBATCH --ntasks-per-node=1		# Specify tasks per node
+#SBATCH -t 100:00:00			# Specify maximum time limit (hour: minute: second)
+#SBATCH -A zz991010			# Specify project name
+#SBATCH -J T12a		# Specify job name
+#SBATCH -o output_train_T12a.txt		# Specify output file name with job ID
+
+module load Miniforge3/25.3.0-3
+conda activate /project/zz991000-zdeva/zz991010/env
+
+export HF_HOME=/project/zz991000-zdeva/zz991010/hf/misc
+export HF_DATASETS_CACHE=/project/zz991000-zdeva/zz991010/hf/datasets
+export TRANSFORMERS_CACHE=/project/zz991000-zdeva/zz991010/hf/models
+export HF_EVALUATE_CACHE=/project/zz991000-zdeva/zz991010/hf/evaluate
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+START=`date`
+starttime=$(date +%s)
+
+srun  python finetune_whisper.py \
+    --model_name typhoon-ai/typhoon-whisper-large-v3 \
+    --output_dir ./whisper-thai-finetuned-t12a \
+    --epochs 10 --learning_rate 5e-6 --augment --freeze_encoder
+
+END=`date`
+endtime=$(date +%s)
+echo "Job start at" $START
+echo "Job end   at" $END
